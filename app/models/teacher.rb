7ccd61fe9659cf620bf107e6d73cc3916742db6f
Alex Wang <ice_b0und@hotmail.com>
2015-10-01 21:27:03 +0800

    add teacher flag

--- app/models/teacher.rb
+++
@@ -1,7 +0,0 @@
-class Teacher < ActiveRecord::Base
-  validates :name, presence: true
-  validates :encrypted_password, presence: true
-
-  has_many :teacher_courses
-  has_many :courses, through: :teacher_courses
-end
