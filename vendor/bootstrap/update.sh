320d83361da9d3b90d7a1cdc12cde27206714ac5
luogan <luogan_62625686@126.com>
2015-10-03 20:48:22 +0800

    添加了一些界面效果

--- vendor/bootstrap/update.sh
+++
@@ -1,5 +0,0 @@
-make bootstrap
-cp bootstrap/css/*.min.css ../../app/assets/stylesheets
-cp bootstrap/js/bootstrap.min.js ../../app/assets/javascripts
-cp bootstrap/img/*.png ../../app/assets/images
-rm -rf bootstrap
