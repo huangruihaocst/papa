1d177762c5a826d96c27c4aa23823debe8fe9626
Alex Wang <ice_b0und@hotmail.com>
2015-10-07 14:48:45 +0800

    fix participation bugs

--- app/models/participation.rb
+++ app/models/participation.rb
@@ -1,8 +1,8 @@
 class Participation < ActiveRecord::Base
 
   validates :role, presence: true
-  validates_associated :users
-  validates_associated :courses
+  validates_associated :user
+  validates_associated :course
 
   belongs_to :user
   belongs_to :course
