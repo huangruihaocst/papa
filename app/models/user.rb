43b14aa5682667fc330706ec8f558c0b1dbcc625
Alex Wang <ice_b0und@hotmail.com>
2015-10-07 14:42:01 +0800

    fix login bugs

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,24 @@ class User < ActiveRecord::Base
 
   has_many :participations
   has_many :courses, through: :participations
+
+  # the following 4 methods allow us to login with phone or email
+  def login=(login)
+    @login = login
+  end
+  def login
+    @login || self.phone || self.email
+  end
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_hash).where(["lower(phone) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      where(conditions.to_hash).first
+    end
+  end
+  def email_required?
+    false
+  end
+
 end
