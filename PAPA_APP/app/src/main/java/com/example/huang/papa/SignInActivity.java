2fddfc0be719ba61df5b056838e5645dcd3a4657
huang <huangruihaocst@126.com>
2015-10-10 23:39:45 +0800

    write a bundle helper

--- PAPA_APP/app/src/main/java/com/example/huang/papa/SignInActivity.java
+++ PAPA_APP/app/src/main/java/com/example/huang/papa/SignInActivity.java
@@ -8,6 +8,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 import com.PapaDataBaseManager.papa.PapaDataBaseManager;
+import com.example.huang.papa.BundleHelper;
 
 public class SignInActivity extends AppCompatActivity {
 
@@ -36,12 +37,10 @@ public class SignInActivity extends AppCompatActivity {
         password = edit_password.getText().toString();
 
         PapaDataBaseManager papaDataBaseManager = PapaDataBaseManager.getInstance();
-        if(papaDataBaseManager.signIn(username, password))
-        {
+        if(papaDataBaseManager.signIn(username, password)){
             check_message = 1;
         }
-        else
-        {
+        else{
             // ToDo: 区分到底是错误密码还是未注册
             check_message = 0;
         }
@@ -52,8 +51,11 @@ public class SignInActivity extends AppCompatActivity {
         if(check_message == 1){
             Intent intent = new Intent(SignInActivity.this,CourseActivity.class);
             Bundle data = new Bundle();
-            String key_sign_in_course_1 = getString(R.string.key_sign_in_course_1);
-            data.putString(key_sign_in_course_1,username);
+            String key_sign_in_course = getString(R.string.key_sign_in_course);
+            BundleHelper bundleHelper = new BundleHelper();
+            bundleHelper.setUsername(username);
+            //TODO: use BundleHelper to send messages
+//            data.putParcelable(key_sign_in_course,bundleHelper);
             intent.putExtras(data);
             startActivity(intent);
         }else if(check_message == 0){
