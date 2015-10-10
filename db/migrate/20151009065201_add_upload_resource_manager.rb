5f3c248c7a33454783f8f9f38e993248dfbc055f
Alex Wang <ice_b0und@hotmail.com>
2015-10-11 01:01:50 +0800

    PAPA-67 db tables migrations

--- db/migrate/20151009065201_add_upload_resource_manager.rb
+++
@@ -1,8 +0,0 @@
-class AddUploadResourceManager < ActiveRecord::Migration
-  def change
-    create_table :upload_files do |t|
-      t.string    :path
-      t.integer   :user_id
-    end
-  end
-end
