20048db479cd451eda5c16ffcc97d5758210d561
shyo <shyoshyo@qq.com>
2015-10-11 07:02:24 +0100

    PAPA-62 编写了用于取得手机号的单元测试(只适用于我的手机)

---
+++ PAPA_APP/app/src/main/java/com/TelephoneInfoManager/test/TelephoneNumberTest.java
@@ -0,0 +1,36 @@
+package com.TelephoneInfoManager.test;
+
+import android.content.Context;
+import android.test.InstrumentationTestCase;
+import com.TelephoneInfoManager.papa.PapaTelephoneNumberGetter;
+
+import android.util.Log;
+import android.widget.Toast;
+
+/**
+ * Created by shyo on 15-10-11.
+ *
+ * ja-JP: 電話番号取得の単体テスト
+ * zh-CN: 获取电话号码的单元测试
+ */
+
+public class TelephoneNumberTest extends InstrumentationTestCase {
+    public void test() throws Exception {
+        Context context = getInstrumentation().getContext();
+        String s = PapaTelephoneNumberGetter.getTelephoneNumber(context);
+
+        if(s == null)
+        {
+            Log.i("TelephoneNumberTest", "zh-CN: 无法获得电话 \n ja-JP: 携帯電話の番号は取得できません。");
+            return;
+        }
+        else
+        {
+            Log.i("TelephoneNumberTest", "zh-CN: 电话是 " + s + "\n ja-JP: 番号は " + s);
+        }
+
+
+        // 翔翔才不会告诉你电话呢瞄
+        assertEquals(s.charAt(s.length()-1), '5');
+    }
+}
