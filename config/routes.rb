7c70eeb18d408f2381855e9dda09cee24b50aefa
Sleepy_chord <sj702d5@foxmail.com>
2015-10-23 12:40:27 +0800

    PAPA-136 speed uop

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
   resources :courses, only: [:show, :update, :destroy] do
     resources :students, only: [:index, :destroy]
     post 'students/:id' => 'students#create', as: :create_student
+    post 'teachers/:id' => 'teachers#create', as: :create_teacher
 
     resources :assistants, only: [:index, :create, :destroy]
     resources :lessons, only: [:index, :create, :destroy]
