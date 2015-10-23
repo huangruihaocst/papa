ab89cf614d7d68bbabd2139fe97853bda4b8a909
Sleepy_chord <sj702d5@foxmail.com>
2015-10-23 12:10:08 +0800

    backup

--- app/controllers/manage_controller.rb
+++ app/controllers/manage_controller.rb
@@ -1,6 +1,6 @@
 class ManageController < ApplicationController
-  layout "main"
-  #before_action :authenticate_user!
+  layout "application"
+  before_action :authenticate_user!
   def index
   end
   def main_page
