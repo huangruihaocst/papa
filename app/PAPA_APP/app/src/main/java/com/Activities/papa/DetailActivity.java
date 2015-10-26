227aaf95160dc92dbf0e0ef4441c449a6bfd3ae1
huang <huangruihaocst@126.com>
2015-10-26 22:13:02 +0800

    ready to merge

--- app/PAPA_APP/app/src/main/java/com/Activities/papa/DetailActivity.java
+++ app/PAPA_APP/app/src/main/java/com/Activities/papa/DetailActivity.java
@@ -134,8 +134,7 @@ public class DetailActivity extends AppCompatActivity
                         Intent intent = new Intent();
                         intent.setType("image/*");
                         intent.setAction(Intent.ACTION_GET_CONTENT);
-                        startActivityForResult(Intent.createChooser(intent,
-                                "Select Picture"), SELECT_PICTURE);
+                        startActivityForResult(Intent.createChooser(intent, "Select Picture"), SELECT_PICTURE);
                     }
                 }
             });
@@ -245,17 +244,16 @@ public class DetailActivity extends AppCompatActivity
 
     public String getPath(Uri uri) {
         // just some safety built in
-        if( uri == null ) {
-            // TODO perform some logging or show user feedback
+        if(uri == null) {
+            // TODO: perform some logging or show user feedback
             return null;
         }
         // try to retrieve the image from the media store first
         // this will only work for images selected from gallery
-        String[] projection = { MediaStore.Images.Media.DATA };
-        Cursor cursor = managedQuery(uri, projection, null, null, null);
-        if( cursor != null ){
-            int column_index = cursor
-                    .getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        String[] projection = {MediaStore.Images.Media.DATA};
+        Cursor cursor = managedQuery(uri,projection,null,null,null);
+        if(cursor != null){
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
             cursor.moveToFirst();
             return cursor.getString(column_index);
         }
