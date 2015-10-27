b0255589942c400357ee9bd9e06c149757ba149d
Sleepy_chord <sj702d5@foxmail.com>
2015-10-28 01:35:07 +0800

    PAPA-53 back to main_page

---
+++ db/app/controllers/manage/manage_controller.rb
@@ -0,0 +1,38 @@
+module  Manage
+class ManageController < ApplicationController
+  layout "application"
+  before_action :authenticate_user!
+  def index
+  end
+  def main_page
+  end
+  def course_score
+    @id= params[:id]
+  end
+  def class_score
+    @id= params[:id]
+  end
+  def course_info
+    @id= params[:id]
+  end
+  def class_info
+    @id= params[:id]
+  end
+  def show_photos
+    @id= params[:id]
+  end
+  def student_lesson_info
+    begin
+      @student = User.find(params[:id])
+      @lesson = Lesson.find(params[:lessonId])
+      @avator = FileResource.find(@student.avator_id)
+    rescue
+      puts "error:#{$!} at:#{$@}"
+      @avator = Object.new
+      def @avator.path
+        "/photos/noAvatar.jpg"
+      end
+    end
+  end
+end
+end
