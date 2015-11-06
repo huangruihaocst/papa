0609a24d9cc9ae49fd4b8ab85daa492dcb6e516f
Alex Wang <ice_b0und@hotmail.com>
2015-11-06 14:25:26 +0800

    PAPA-171 try https

--- db/config/environments/development.rb
+++ db/config/environments/development.rb
@@ -43,4 +43,7 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   config.consider_all_requests_local = false
+
+  config.to_prepare { Devise::SessionsController.force_ssl }
+  config.to_prepare { Devise::RegistrationsController.force_ssl }
 end
