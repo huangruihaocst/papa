7fa54c42c2feb8b9291f7afacad40b2ee47cb3a5
huang <huangruihaocst@126.com>
2015-11-12 04:36:00 +0800

    PAPA-219 fixed

--- app/PAPA_APP/app/src/main/java/com/Fragments/papa/ExperimentResultFragment.java
+++ app/PAPA_APP/app/src/main/java/com/Fragments/papa/ExperimentResultFragment.java
@@ -33,10 +33,13 @@ import com.Back.NetworkAccess.papa.PapaHttpClientException;
 import com.Back.PapaDataBaseManager.papa.PapaDataBaseManager;
 
 import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -262,6 +265,7 @@ public class ExperimentResultFragment extends Fragment {
                         }else if(mimeType.contains("image")){
                             Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath());
 //                    selectedImage.setImageBitmap(bitmap);
+                            bitmap = ThumbnailUtils.extractThumbnail(bitmap,200,200);
                             bitmapArrayList.add(bitmap);
                             pathArrayList.add(path);
                             isImageArrayList.add(1);
@@ -296,6 +300,7 @@ public class ExperimentResultFragment extends Fragment {
                     if(file.exists()){
                         Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath());
 //                        selectedImage.setImageBitmap(bitmap);
+                        bitmap = ThumbnailUtils.extractThumbnail(bitmap,200,200);
                         bitmapArrayList.add(bitmap);
                         pathArrayList.add(path);
                         isImageArrayList.add(1);
@@ -458,22 +463,22 @@ public class ExperimentResultFragment extends Fragment {
         return mediaFile;
     }
 
-    public byte[] toByteArray(File file){
-        int size = (int) file.length();
-        byte[] bytes = new byte[size];
-        try {
-            BufferedInputStream buf = new BufferedInputStream(new FileInputStream(file));
-            buf.read(bytes, 0, bytes.length);
-            buf.close();
-        } catch (FileNotFoundException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        return bytes;
-    }
+//    public byte[] toByteArray(File file){
+//        int size = (int) file.length();
+//        byte[] bytes = new byte[size];
+//        try {
+//            BufferedInputStream buf = new BufferedInputStream(new FileInputStream(file));
+//            buf.read(bytes, 0, bytes.length);
+//            buf.close();
+//        } catch (FileNotFoundException e) {
+//            // TODO Auto-generated catch block
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            // TODO Auto-generated catch block
+//            e.printStackTrace();
+//        }
+//        return bytes;
+//    }
 
     public class ImageAdapter extends BaseAdapter{
 
