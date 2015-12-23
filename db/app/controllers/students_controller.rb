04624ca8252356db8b7547983302691bc972ca94
Alex Wang <ice_b0und@hotmail.com>
2015-12-23 14:41:50 +0800

    fix dup student no

--- db/app/controllers/students_controller.rb
+++ db/app/controllers/students_controller.rb
@@ -81,7 +81,7 @@ class StudentsController < ApplicationController
             raise RequestException.new(REASON_INVALID_FIELD) unless student['student_number'] || student['name'] || student['email'] || student['phone']
             exist_user = User.find_by_student_number(student['student_number'])
             if exist_user
-              course.add_student(exist_user) if exist_user.courses.include?(course)
+              #course.add_student(exist_user) if exist_user.courses.include?(course)
             else
               begin
                 user = User.create(name: student['name'],
