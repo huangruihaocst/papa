d7613bd874226c9b5dfc76e90e343f1c121a33ad
Alex Wang <ice_b0und@hotmail.com>
2016-03-16 12:50:24 +0800

    add default admin

--- db/db/seeds.rb
+++ db/db/seeds.rb
@@ -5,4 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-require './db/init_data/db'
+#require './db/init_data/db'
+
+# create default admin and default avator
+admin0 = User.create(name:'admin', phone:'0123', email:'a@b.c', password:'123', password_confirmation:'123',
+                     student_number: '1230', description: '123', is_admin: true, avator_id: 1)
+default_avator = FileResource.create(path: '/uploads/default_avator.jpg', name: 'default_avator.jpg', file_type: FILE_TYPE_IMAGE, creator_id: admin0.id)
+admin0.avator_id = default_avator.id
+admin0.save
+puts 'admin created...'
+
